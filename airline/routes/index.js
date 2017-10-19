47a7fa1fd0ad3d4a4d50e6ff82b4b40eb306a42f
hak <hammad4898@gmail.com>
2017-10-19 12:59:20 +0500

    Decouple of code.

--- airline/routes/index.js
+++ airline/routes/index.js
@@ -3,54 +3,58 @@
  * GET home page.
  */
 
-// Flights are data small db
-var flights = require('../data/index');
+module.exports = function (flights) {
 
-// flight is object blue print
-var flight = require('../Flight/index');
+    // flight is object blue print
+    var flight = require('../Flight/index');
 
-for (var p in flights){
-    flights[p] = flight(flights[p]);
-}
-
-// See data
-//console.log(flights);
-
-exports.flight = function (req, res) {
-    var num = req.param('number');
-    if (typeof flights[num] === 'undefined'){
-        res.status(404).json({status:'Not found.'});
+    for (var p in flights) {
+        flights[p] = flight(flights[p]);
     }
-    else{
-        res.json(200, flights[num].getinfo());
-    }
+
+    // See data
+    //console.log(flights);
+
+    var functions = {};
+    functions.flight = function (req, res) {
+        var num = req.param('number');
+        if (typeof flights[num] === 'undefined') {
+            res.status(404).json({status: 'Not found.'});
+        }
+        else {
+            res.json(200, flights[num].getinfo());
+        }
+    };
+
+    functions.arrive = function (req, res) {
+        var num = req.param('number');
+        if (typeof flights[num] === 'undefined') {
+            res.status(404).json({status: 'Not found.'});
+        }
+        else {
+            flights[num].triggerArrival();
+            res.status(200).json({status: 'Success'});
+        }
+    };
+
+    functions.depart = function (req, res) {
+        var num = req.param('number');
+        if (typeof flights[num] === 'undefined') {
+            res.status(404).json({status: 'Not Found'});
+        }
+        else {
+            flights[num].triggerDepart();
+            res.status(200).json({status: "Success"});
+        }
+    };
+
+    functions.list = function (req, res) {
+        res.render('list', {title: "AIRLINE", flights: flights});
+    };
+
+    return functions;
 };
 
-exports.arrive = function (req, res) {
-    var num = req.param('number');
-    if (typeof flights[num] === 'undefined'){
-        res.status(404).json({status:'Not found.'});
-    }
-    else{
-        flights[num].triggerArrival();
-        res.status(200).json({status:'Success'});
-    }
-};
-
-exports.depart = function (req, res) {
-    var num = req.param('number');
-    if (typeof flights[num] === 'undefined'){
-        res.status(404).json({status:'Not Found'});
-    }
-    else {
-        flights[num].triggerDepart();
-        res.status(200).json({status:"Success"});
-    }
-};
-
-exports.list = function (req, res) {
-    res.render('list', {title: "AIRLINE", flights: flights});
-};
 
 /*
 exports.flight1 = function (req, res) {
--- airline/server.js
+++ airline/server.js
@@ -1,5 +1,8 @@
 var http = require('http');
-var app = require('./app');
+
+// Flights are data small db
+var flights = require('./data/index');
+var app = require('./app')(flights);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
